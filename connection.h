5b43c34e34b66c7ef5c86a4da52bb82a1586cd47
swpd <swpdtz@gmail.com>
2013-06-28 21:17:18 +0800

    MariaDB: init commit (draft APIs)
    Signed-off-by: swpd <swpdtz@gmail.com>

---
+++ connection.h
@@ -0,0 +1,99 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Monkey HTTP Daemon
+ *  ------------------
+ *  Copyright (C) 2013, Zeying Xie <swpdtz at gmail dot com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef MARIADB_CONNECTION_H
+#define MARIADB_CONNECTION_H
+
+#include <mysql/mysql.h>
+#include "query.h"
+
+#define MARIADB_OK 0
+#define MARIADB_ERR -1
+
+typedef enum {
+    CONN_STATE_NULL, CONN_STATE_CLOSE, CONN_STATE_CLOSED,
+    CONN_STATE_CONNECT, CONN_STATE_CONNECTING, CONN_STATE_CONNECTED,
+    CONN_STATE_QUERY, CONN_STATE_QUERYING, CONN_STATE_QUERIED,
+    CONN_STATE_ROW_STREAM, CONN_STATE_ROW_STREAMING, CONN_STATE_ROW_STREAMED,
+    CONN_STATE_NEXTQUERY, CONN_STATE_NEXTQUERYING,
+    CONN_STATE_RESULT_FREE, CONN_STATE_RESULT_FREEING, CONN_STATE_RESULT_FREED,
+    CONN_STATE_ERR_ROW, CONN_STATE_ERR_QUERY, CONN_STATE_ABORT
+} mariadb_conn_state_t;
+
+typedef struct mariadb_conn_config {
+    char *user;
+    char *password;
+    char *ip;
+    char *db;
+    char *unix_socket;
+    unsigned int port;
+    unsigned long client_flag;
+
+    //ssl config
+    char *ssl_key;
+    char *ssl_cert;
+    char *ssl_ca;
+    char *ssl_capath;
+    char *ssl_cipher;
+} mariadb_conn_config_t;
+
+struct mariadb_conn; /* forward declaration */
+typedef void (mariadb_connect_cb)(struct mariadb_conn *conn, int status);
+typedef void (mariadb_disconnect_cb)(struct mariadb_conn *conn, int status);
+
+typedef struct mariadb_conn {
+    mariadb_conn_config_t config;
+    MYSQL *mysql, *mysql_ret;
+    int fd;
+    mariadb_conn_state_t state;
+
+    mariadb_connect_cb *connect_cb;
+    mariadb_disconnect_cb *disconnect_cb;
+
+    struct mk_list queries;
+} mariadb_conn_t;
+
+static inline int mariadb_set_connect_cb(mariadb_conn_t *conn, mariadb_connect_cb *cb)
+{
+    if (conn->connect_cb == NULL)
+    {
+        conn->connect_cb = cb;
+        return MARIADB_OK;
+    }
+    return MARIADB_ERR;
+}
+
+static inline int mariadb_set_disconnect_cb(mariadb_conn_t *conn, mariadb_disconnect_cb *cb)
+{
+    if (conn->disconnect_cb == NULL)
+    {
+        conn->disconnect_cb = cb;
+        return MARIADB_OK;
+    }
+    return MARIADB_ERR;
+}
+
+int mariadb_connect(mariadb_conn_t *conn);
+int mariadb_disconnect(mariadb_conn_t *conn);
+int mariadb_conn_add_query(mariadb_conn_t *conn, struct duda_request *dr,
+                           char *query_str, mariadb_query_cb *cb, void *privdata);
+
+#endif
---
+++ mariadb.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Monkey HTTP Daemon
+ *  ------------------
+ *  Copyright (C) 2013, Zeying Xie <swpdtz at gmail dot com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef DUDA_PACKAGE_MARIADB_H
+#define DUDA_PACKAGE_MARIADB_H
+
+#include "connection.h"
+
+pthread_key_t mariadb_conn_list;
+
+typedef struct duda_api_mariadb {
+    int (*connect)(mariadb_conn_t *);
+    int (*disconnect)(mariadb_conn_t *);
+    int (*set_connect_cb)(mariadb_conn_t *, mariadb_connect_cb *);
+    int (*set_disconnect_cb)(mariadb_conn_t *, mariadb_disconnect_cb *);
+    int (*query)(mariadb_conn_t *, struct duda_request *, char *, mariadb_query_cb *, void *);
+    int (*abort)(mariadb_query_t *);
+} mariadb_object_t;
+
+mariadb_object_t *mariadb;
+
+static inline int mariadb_init_keys()
+{
+    if (pthread_key_create(&mariadb_conn_list, NULL) != 0)
+        return MARIADB_ERR;
+    return MARIADB_OK;
+}
+
+int mariadb_read(int fd, struct duda_request *dr);
+int mariadb_write(int fd, struct duda_request *dr);
+int mariadb_error(int fd, struct duda_request *dr);
+int mariadb_close(int fd, struct duda_request *dr);
+int mariadb_timeout(int fd, struct duda_request *dr);
+
+#endif
